41623d4d434e016b2f6b4379ad9b937fb94fe698
harrynoske-tech <harry.noske@gmail.com>
2026-07-18 16:10:26 +1000

    Update gameConfig.js

--- config/gameConfig.js
+++ config/gameConfig.js
@@ -1,1 +1,31 @@
+const gameConfig = {
+    type: Phaser.AUTO,
 
+    backgroundColor: "#111111",
+
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1280,
+        height: 720
+    },
+
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+
+    scene: [
+        BootScene,
+        PreloadScene,
+        MenuScene,
+        GameScene,
+        PauseScene,
+        SettingsScene,
+        ShopScene,
+        VictoryScene,
+        GameOverScene
+    ]
+};
